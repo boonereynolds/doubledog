349545b67e93c5610ee5d3d650c08ca65e368051
Gus Nordhielm <gnordhielm@gmail.com>
2017-04-21 14:53:07 -0500

    add hard-coded completed dares

--- www/js/services.js
+++ www/js/services.js
@@ -16,15 +16,15 @@ angular.module('starter.services', [])
 
   return {
     all: function() {
-      console.log("Getting all dares", dares)
+      console.log("Getting all hard-coded dares", dares)
       return dares;
     },
     remove: function(dare) {
-      console.log("Removing dare with id", dare)
+      console.log("Removing hard-coded dare with id", dare)
       dares.splice(dares.indexOf(dare), 1);
     },
     get: function(dareId) {
-      console.log("Looking for dare with id", dareId)
+      console.log("Looking for hard-coded dare with id", dareId)
       for (var i = 0; i < dares.length; i++) {
         if (dares[i].id === parseInt(dareId)) {
           return dares[i];
@@ -33,4 +33,56 @@ angular.module('starter.services', [])
       return null;
     }
   };
-});
+}
+
+.factory('CompletedDares', function() {
+  var completedDares = [{
+    id:0,
+    
+    dare_id: 0,
+    user_id: 0,
+    picture_url: '../img/adam.jpg',
+    completed_at: new Date()
+  },{
+    id:1,
+
+    dare_id: 1,
+    user_id: 1,
+    picture_url: '../img/ben.jpg',
+    completed_at: new Date()
+  }{
+    id:2,
+
+    dare_id: 0,
+    user_id: 2,
+    picture_url: '../img/max.jpg',
+    completed_at: new Date()
+  }];
+
+  return {
+    all: function() {
+      console.log("Getting all hard-coded completedDares", completedDares)
+      return completedDares;
+    },
+    remove: function(dare) {
+      console.log("Removing hard-coded dare with id", dare)
+      completedDares.splice(completedDares.indexOf(dare), 1);
+    },
+    get: function(dareId) {
+      console.log("Looking for hard-coded dare with id", dareId)
+      for (var i = 0; i < completedDares.length; i++) {
+        if (completedDares[i].id === parseInt(dareId)) {
+          return completedDares[i];
+        }
+      }
+      return null;
+    }
+  };
+}
+
+);
+
+// This code is for firebase integration. It occurred to me that this might be confusing,
+// so you can leave it out and just demo the hard-coded stuff. Keep in mind, the brackets
+// are not matched up. If you want to switch to this code, you need to delete everything 
+// from this comment to line 6.
